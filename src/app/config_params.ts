1ea4fce045d42df480e3dfd3b6835942ee337345
kalud <khaled.fhm@live.fr>
2021-05-05 13:33:21 +0200

    cleaned basics.js

---
+++ src/app/config_params.ts
@@ -0,0 +1,153 @@
+///////////////////////////////////////
+// config parameters and files
+///////////////////////////////////////
+
+import { states_struct, status_clearall, struct } from './basics';
+
+export const RE_TOKEN_DELIMS = /[;\n]+/;
+export const TOKEN_DELIMITER = ";"
+export const NO_LIMIT = 'U';
+export const FLOAT_FRACTION_DIGITS = 3;
+
+export const cfg_parameters_states = [
+  states_struct('CFG_PARAMETERS_STATE_COUNT',		0),
+  states_struct('CFG_PARAMETERS_STATE_VALUES',	0),
+];
+
+export const cfg_parameter_value_struct = struct(
+  'name',
+  'current_value',
+  'text',
+  'type',
+  'allowed_values'
+);
+
+export var cfg_parameter_value = [];
+export var	cfg_parameter_count = 0;
+
+
+export  function check_parameter(parameter) {
+  var i = cfg_parameter_value.findIndex(element => "parameter_input_" + element.name == parameter.id);
+  var name = cfg_parameter_value[i].name;
+  var input_id = parameter.id;
+  var input_id = 'parameter_input_' + name;
+  var value_old = cfg_parameter_value[i].current_value;
+
+  switch (cfg_parameter_value[i].type) {
+    case "L":
+      // list: correct by design
+      break;
+    case "I":
+      // int: auto correct values if limits exceeded
+      var value = parseInt($("#" + input_id).val());
+      if (isNaN(value)) {
+        value = 0;
+        $("#" + input_id).val(value);
+      }
+      var min = cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[0];
+      var max = cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[1];
+      if (min != NO_LIMIT && value < parseInt(min))
+        $("#" + input_id).val(parseInt(min));
+      if (max != NO_LIMIT && value > parseInt(max))
+        $("#" + input_id).val(parseInt(max));
+      break;
+    case "F":
+      // float: auto correct values if limits exceeded
+      var value = parseFloat($("#" + input_id).val());
+      if (isNaN(value)) value = 0;
+      var min = cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[0];
+      var max = cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[1];
+      if (min != NO_LIMIT && value < parseFloat(min))
+        $("#" + input_id).val(parseFloat(min));
+      if (max != NO_LIMIT && value > parseFloat(max))
+        $("#" + input_id).val(parseFloat(max));
+      // show float values in exponential number format
+      $("#" + input_id).val(parseFloat($("#" + input_id).val()).toExponential(FLOAT_FRACTION_DIGITS));
+      break;
+    default:
+      console.log("ERROR: Unknown type " + cfg_parameter_value[i].type + " for cfg parameter " + name);
+  }
+
+  // request update of cfg parameter value only if parameter value changed
+  var value = $("#" + input_id).val();
+  if (value_old != value) {
+    send_cmd(rc_cmds.CMD_RC_CFG_VALUE, name, value);
+
+    // clear name of currently loaded cfg file if a parameter value has changed
+    cfg_file_current = '';
+    $("#ctl_cfg_file_load_name").val("");
+  }
+
+  // store new parameter value
+  cfg_parameter_value[i].current_value = $("#" + input_id).val();
+}
+
+
+export function update_parameters(){
+  // remove old cfg parameter labels and inputs
+  $("#grid_parameters_container *").remove();
+
+  // generate full set of cfg parameter labels and inputs
+  for (var i in cfg_parameter_value) {
+    var name = cfg_parameter_value[i].name;
+    var value = cfg_parameter_value[i].current_value;
+    var div_id = 'parameter_' + name;
+    var input_id = 'parameter_input_' + name;
+    var label_text = cfg_parameter_value[i].text + ": ";
+    $("#grid_parameters_container").append("<div id='" + div_id + "'></div>");
+    $("#" + div_id).append("<label for='" + input_id + "'>" + label_text + "</label>");
+    switch (cfg_parameter_value[i].type) {
+      case "I":
+        // int
+        $("#" + div_id).append("<input type='number' name='" + input_id + "' id='" + input_id + "' onchange=check_parameter(this)>");
+        $("#" + input_id).attr('min', parseFloat(cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[0]));
+        $("#" + input_id).attr('max', parseFloat(cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[1]));
+        $("#" + input_id).val(Math.floor(value));
+        break;
+      case "L":
+        // list
+        $("#" + div_id).append("<select id='" + input_id + "' onchange=check_parameter(this)></select>");
+        for (var k in cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)) {
+          $("#" + input_id).append("<option>" + cfg_parameter_value[i].allowed_values.split(RE_TOKEN_DELIMS)[k] + "</option>");
+        }
+        $("#" + input_id).val(value);
+        break;
+      case "F":
+        // float
+        $("#" + div_id).append("<input type='text' name='" + input_id + "' id='" + input_id + "' onchange=check_parameter(this)>");
+        $("#" + input_id).val(parseFloat(value).toExponential(FLOAT_FRACTION_DIGITS));
+        break;
+      default:
+        console.log("ERROR: Unknown type " + cfg_parameter_value[i].type + " for cfg parameter " + name);
+    }
+    switch (rc_state) {
+      case rc_states.RC_STATE_QUIT:
+      case rc_states.RC_STATE_UNDEFINED:
+      case rc_states.RC_STATE_GO:
+        $("#" + input_id).attr("disabled", "disabled");
+        break;
+      case rc_states.RC_STATE_HALT:
+        $("#" + input_id).removeAttr("disabled");
+        if ($('#ctl_trigger_source').val() != "Internal") {
+          $('#parameter_input_timer_period_ms').attr('disabled', 'disabled');
+        }
+        break;
+      default:
+        console.log("ERROR: Unknown state: " + rc_state);
+        break;
+    }
+  }
+}
+
+
+export function init_parameters() {
+  cfg_parameter_count = 0;
+  cfg_parameter_value = [];
+
+  status_clearall(cfg_parameters_states);
+}
+
+
+
+
+
---
+++ src/app/headers.ts
@@ -0,0 +1,202 @@
+///////////////////////////////////////
+// headers, see buffers.h for details
+///////////////////////////////////////
+
+import { arraybuffer2string, status_iscomplete, status_isset, struct } from './basics';
+
+
+
+// header tags
+const HEADER_TAG_PIPELINE = "PH";
+const HEADER_TAG_NODE = "NH";
+const HEADER_TAG_DATA = "DH";
+const HEADER_TAG_COMMAND = "CH";
+
+// user data in headers
+const HEADER_USER_DATA_SIZE = 2;
+
+// type header
+const TYPE_HEADER_SIZE_BYTES = 4 + 4 * HEADER_USER_DATA_SIZE;  //being used outside as well
+
+const type_header_struct = struct(
+  'tag',
+  'value',
+  'user_data'
+);
+
+
+// function print_header(header) {
+// 	for (var key in header) {
+// 		console.log(key + ": " + header[key]);
+// 	}
+// }
+
+function arraybuffer2type_header(arraybuffer) { //being used outside as well
+  var dv = new DataView(arraybuffer);
+  var offset_tag = 0;
+  var offset_value = 2;
+  var offset_user_data = 4;
+
+  return type_header_struct(
+    arraybuffer2string(arraybuffer.slice(offset_tag, offset_value)),
+    dv.getInt16(offset_value, true),
+    arraybuffer.slice(offset_user_data)
+  );
+}
+
+// node header
+const NODE_HEADER_SIZE_BYTES = 100 + 4 * HEADER_USER_DATA_SIZE;
+const NODE_LABEL_SIZE_BYTES = 31;
+const node_header_struct = struct(
+  'tag',
+  'mode',
+  'label_x',
+  'label_y',
+  'datatype',
+  'bitwidth',
+  'image_size_x',
+  'image_start_x',
+  'image_range_x',
+  'image_size_y',
+  'image_start_y',
+  'image_range_y',
+  'world_start_x',
+  'world_range_x',
+  'world_start_y',
+  'world_range_y',
+  'bufadr_p',
+  'user_data'
+);
+
+var nodes_headers;
+
+
+function arraybuffer2node_header(arraybuffer, node_index) {
+  var dv = new DataView(arraybuffer);
+
+  var offset_tag = TYPE_HEADER_SIZE_BYTES + node_index * NODE_HEADER_SIZE_BYTES;
+  var offset_mode = offset_tag + 2;
+  var offset_labels = offset_mode + 2;
+  var offset_datatype = offset_labels + 2 * NODE_LABEL_SIZE_BYTES;
+  var offset_bitwidth = offset_datatype + 1;
+  var offset_image = offset_bitwidth + 1;
+  var offset_world = offset_image + 12;
+  var offset_bufadr = offset_world + 16;
+  var offset_user_data = offset_bufadr + 4;
+
+  return node_header_struct(
+    arraybuffer2string(arraybuffer.slice(offset_tag, offset_tag + 2)),
+    dv.getUint16(offset_mode, true),
+    arraybuffer2string(arraybuffer.slice(offset_labels, offset_labels + NODE_LABEL_SIZE_BYTES)),
+    arraybuffer2string(arraybuffer.slice(offset_labels + NODE_LABEL_SIZE_BYTES, offset_labels + 2 * NODE_LABEL_SIZE_BYTES)),
+    arraybuffer2string(arraybuffer.slice(offset_datatype, offset_datatype + 1)),
+    dv.getUint8(offset_bitwidth, true),
+    dv.getUint16(offset_image, true),
+    dv.getUint16(offset_image + 2, true),
+    dv.getUint16(offset_image + 4, true),
+    dv.getUint16(offset_image + 6, true),
+    dv.getUint16(offset_image + 8, true),
+    dv.getUint16(offset_image + 10, true),
+    dv.getFloat32(offset_world, true),
+    dv.getFloat32(offset_world + 4, true),
+    dv.getFloat32(offset_world + 8, true),
+    dv.getFloat32(offset_world + 12, true),
+    dv.getUint32(offset_bufadr, true),
+    arraybuffer.slice(offset_user_data)
+  );
+}
+
+
+function process_pipeline_headers(data_buffer) {
+  var number_of_nodes = arraybuffer2type_header(data_buffer.slice(0, TYPE_HEADER_SIZE_BYTES)).value;
+  nodes_headers = [];
+  for (var i = 0; i < number_of_nodes; i++) {
+    nodes_headers[i] = arraybuffer2node_header(data_buffer, i);
+// 		print_header(nodes_headers[i], i);
+  }
+  // cfg_parameters and controls must be fully configured before
+  if ((!status_iscomplete(cfg_parameters_states)) ||
+    (!status_iscomplete(controls_states))) {
+    // reset cfg_parameters/controls, request initial config
+    console.log("WARNING: Pipeline header received, but basic configuration is not complete");
+    init_parameters();
+    init_controls();
+    request_initial_config();
+    return;
+  }
+
+  // nodes names and nodes output modes must be configured before
+  if ((!status_isset(graphics_states, 'GRAPHICS_STATE_NODES_NAMES')) ||
+    (!status_isset(graphics_states, 'GRAPHICS_STATE_NODES_OMODES'))) {
+    // reset graphics, request nodes names/omodes
+    console.log("WARNING: Pipeline header received, but nodes names/output configuration not complete");
+    init_graphics();
+    send_cmd(rc_cmds.CMD_RC_NODES_NAMES);
+    send_cmd(rc_cmds.CMD_RC_NODES_OMODES);
+    return;
+  }
+
+  // no nodes to be displayed -> setup graphics with default values
+  if (node_index_required < 0) {
+    scale_legend_x = 'range';
+    scale_legend_y = 'velocity';
+    data_datatype = 'U';
+    data_bitwidth = 8;
+    data_image_size_x = 1;
+    data_image_size_y = 1;
+    data_image_start_x = 0;
+    data_image_start_y = 0;
+    data_image_range_x = 0;
+    data_image_range_y = 0;
+    scale_world_start_x = 0;
+    scale_world_start_y = 0;
+    scale_world_range_x = 0;
+    scale_world_range_y = 0;
+    res_x = 0;
+    res_y = 0;
+    status_set(graphics_states, 'GRAPHICS_STATE_SCALES');
+    update_graphics();
+    return;
+  }
+
+  // take the new scales parameters if parameters changed
+  var scale_parameters_changed = (
+    (scale_legend_x != nodes_headers[node_index_required].label_x) ||
+    (scale_legend_y != nodes_headers[node_index_required].label_y) ||
+    (data_datatype != nodes_headers[node_index_required].datatype) ||
+    (data_bitwidth != nodes_headers[node_index_required].bitwidth) ||
+    (data_image_size_x != nodes_headers[node_index_required].image_size_x) ||
+    (data_image_size_y != nodes_headers[node_index_required].image_size_y) ||
+    (data_image_start_x != nodes_headers[node_index_required].image_start_x) ||
+    (data_image_start_y != nodes_headers[node_index_required].image_start_y) ||
+    (data_image_range_x != nodes_headers[node_index_required].image_range_x) ||
+    (data_image_range_y != nodes_headers[node_index_required].image_range_y) ||
+    (scale_world_start_x != nodes_headers[node_index_required].world_start_x) ||
+    (scale_world_start_y != nodes_headers[node_index_required].world_start_y) ||
+    (scale_world_range_x != nodes_headers[node_index_required].world_range_x) ||
+    (scale_world_range_y != nodes_headers[node_index_required].world_range_y)
+  );
+  if (scale_parameters_changed) {
+    scale_legend_x = nodes_headers[node_index_required].label_x;
+    scale_legend_y = nodes_headers[node_index_required].label_y;
+    data_datatype = nodes_headers[node_index_required].datatype;
+    data_bitwidth = nodes_headers[node_index_required].bitwidth;
+    data_image_size_x = nodes_headers[node_index_required].image_size_x;
+    data_image_size_y = nodes_headers[node_index_required].image_size_y;
+    data_image_start_x = nodes_headers[node_index_required].image_start_x;
+    data_image_start_y = nodes_headers[node_index_required].image_start_y;
+    data_image_range_x = nodes_headers[node_index_required].image_range_x;
+    data_image_range_y = nodes_headers[node_index_required].image_range_y;
+    scale_world_start_x = nodes_headers[node_index_required].world_start_x;
+    scale_world_start_y = nodes_headers[node_index_required].world_start_y;
+    scale_world_range_x = nodes_headers[node_index_required].world_range_x;
+    scale_world_range_y = nodes_headers[node_index_required].world_range_y;
+    res_x = scale_world_range_x / (data_image_range_x - 1);
+    res_y = scale_world_range_y / (data_image_range_y - 1);
+// 		console.log("New scale parameters applied");
+    status_set(graphics_states, 'GRAPHICS_STATE_SCALES');
+    update_graphics();
+  }
+}
+
+
